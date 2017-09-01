e5e9a67d3d4e7579eee59e49988c728f478c8652
Jake Dobson <jake.dobson@gmail.com>
2017-09-01 11:36:18 -0700

    challenge finished

--- PrimitiveScene.swift
+++ PrimitiveScene.swift
@@ -4,9 +4,11 @@
 //  Copyright © 2017 Jacob Dobson. All rights reserved.
 
 /*
-	-- Make a 20x20 tangent grid
-	-- Assume that each sphere has a row and column number from 0 to 19
-	-- Use the formula sphereColor = i & j != 0 ? UIColor.purpleColor() : UIColor.orangeColor()
+	-- Make a triangle out of spheres
+	-- Make the spheres tangent to each other
+
+	* Hint * Increase the y-coordinate at each step sqrt(3) --> height of equilateral triangle
+		https://camo.githubusercontent.com/76f62e282f21730fb94f178acb9443957d42e052/687474703a2f2f7777772e7765686561727473776966742e636f6d2f77702d636f6e74656e742f75706c6f6164732f323031342f31302f657175696c61746572616c2d747269616e676c652e706e67
 */
 
 import UIKit
@@ -23,12 +25,15 @@ class PrimitiveScene: SCNScene {
 		let yCount = 20
 		//create 20 spheres in for loop using an index
 		for row in 0..<yCount {
-			var x: Float = 0.0
-			for column in 0..<xCount {
+			var x: Float = Float(radius) * Float(row)
+			for column in row..<xCount {
 				let sphere = SCNSphere(radius: radius)
 				//color spheres based on row/column
-				sphere.firstMaterial?.diffuse.contents = row & column != 0 ? UIColor.green : UIColor.red
-				print(row, column)
+				if ((row + column) % 2 == 0) {
+					sphere.firstMaterial?.diffuse.contents = UIColor.green
+				} else {
+					sphere.firstMaterial?.diffuse.contents = UIColor.red
+				}
 				//set node and position
 				let sphereNode = SCNNode(geometry: sphere)
 				sphereNode.position = SCNVector3(x: x, y: y, z: 0)
@@ -37,7 +42,7 @@ class PrimitiveScene: SCNScene {
 				//distance b/w spheres
 				x += Float(radius)*2
 			}
-			y += Float(radius)*2
+			y += sqrt(3) * Float(radius)
 		}
 	}
 	//init w/ coder method
