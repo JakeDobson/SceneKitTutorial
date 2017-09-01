5f9c4b17ad262e09e9f8cdb439d24ad17b00ab3b
Jake Dobson <jake.dobson@gmail.com>
2017-08-31 17:46:20 -0700

    finished challenge

--- PrimitiveScene.swift
+++ PrimitiveScene.swift
@@ -4,9 +4,9 @@
 //  Copyright © 2017 Jacob Dobson. All rights reserved.
 
 /*
-	-- Make a grid of 20×20 spheres of radius 0.1 in the XY plane
-	-- Make each sphere tangent to it’s neighbors
-	-- Color the spheres in a grid pattern with orange and purple
+	-- Make a 20x20 tangent grid
+	-- Assume that each sphere has a row and column number from 0 to 19
+	-- Use the formula sphereColor = i & j != 0 ? UIColor.purpleColor() : UIColor.orangeColor()
 */
 
 import UIKit
@@ -26,13 +26,9 @@ class PrimitiveScene: SCNScene {
 			var x: Float = 0.0
 			for column in 0..<xCount {
 				let sphere = SCNSphere(radius: radius)
-				//color sphere green if index is odd, red if index is even
-				if (row + column) % 2 == 0 {
-					print(row + column)
-					sphere.firstMaterial?.diffuse.contents = UIColor.green
-				} else {
-					sphere.firstMaterial?.diffuse.contents = UIColor.red
-				}
+				//color spheres based on row/column
+				sphere.firstMaterial?.diffuse.contents = row & column != 0 ? UIColor.green : UIColor.red
+				print(row, column)
 				//set node and position
 				let sphereNode = SCNNode(geometry: sphere)
 				sphereNode.position = SCNVector3(x: x, y: y, z: 0)
