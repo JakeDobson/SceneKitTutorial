b43f279222af2886119295e1d2f49402c41deea3
Jake Dobson <jake.dobson@gmail.com>
2017-08-31 17:37:01 -0700

    finished challenge

--- PrimitiveScene.swift
+++ PrimitiveScene.swift
@@ -4,8 +4,9 @@
 //  Copyright © 2017 Jacob Dobson. All rights reserved.
 
 /*
-	-- Create a line of 20 spheres tangent (just touching) to each of their neighbors
-	-- Make the radius of each sphere 0.01 smaller than the sphere to it’s left.
+	-- Make a grid of 20×20 spheres of radius 0.1 in the XY plane
+	-- Make each sphere tangent to it’s neighbors
+	-- Color the spheres in a grid pattern with orange and purple
 */
 
 import UIKit
@@ -16,25 +17,31 @@ class PrimitiveScene: SCNScene {
 	override init() {
 		super.init()
 		//variables
-		var x: Float = 0.0
-		var radius: CGFloat = 0.25
+		var y: Float = 0.0
+		let radius: CGFloat = 0.1
+		let xCount = 20
+		let yCount = 20
 		//create 20 spheres in for loop using an index
-		for i in 0..<20 {
-			let sphere = SCNSphere(radius: radius)
-			//color sphere red if index is odd, green if index is even
-			if (i % 2 == 0) {
-				sphere.firstMaterial?.diffuse.contents = UIColor.green
-			} else {
-				sphere.firstMaterial?.diffuse.contents = UIColor.red
+		for row in 0..<yCount {
+			var x: Float = 0.0
+			for column in 0..<xCount {
+				let sphere = SCNSphere(radius: radius)
+				//color sphere green if index is odd, red if index is even
+				if (row + column) % 2 == 0 {
+					print(row + column)
+					sphere.firstMaterial?.diffuse.contents = UIColor.green
+				} else {
+					sphere.firstMaterial?.diffuse.contents = UIColor.red
+				}
+				//set node and position
+				let sphereNode = SCNNode(geometry: sphere)
+				sphereNode.position = SCNVector3(x: x, y: y, z: 0)
+				//add node to scene
+				self.rootNode.addChildNode(sphereNode)
+				//distance b/w spheres
+				x += Float(radius)*2
 			}
-			//set node and position
-			let sphereNode = SCNNode(geometry: sphere)
-			sphereNode.position = SCNVector3(x: x, y: 0, z: 0)
-			//add node to scene
-			self.rootNode.addChildNode(sphereNode)
-			//distance b/w spheres
-			radius -= 0.01
-			x += Float(radius)*2
+			y += Float(radius)*2
 		}
 	}
 	//init w/ coder method
